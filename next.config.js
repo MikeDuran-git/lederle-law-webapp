91c8ac13d710b6b1c9705526d7920f2d8836a8b0
Mike Duran <74139042+MikeDuran-git@users.noreply.github.com>
2025-05-01 15:37:50 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  target: 'server',
+  images: { 
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
